0eaefd7b605527f1f95c1d6314b8118d9a3b66bf
Wendler Eis <wendler_eis@hotmail.com>
2023-11-03 00:16:39 -0300

    feat: add user list integration

--- src/api/user.js
+++ src/api/user.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export function createUser(user) {
   return axios.post("/api/user", user);
 }
+
+export function getUsers() {
+  return axios.get("/api/user");
+}
--- src/pages/api/user/index.js
+++ src/pages/api/user/index.js
@@ -8,6 +8,12 @@ export default async function handler(req, res) {
 
     res.send();
   }
+
+  if (req.method === "GET") {
+    const users = await getUsers();
+
+    res.json(users);
+  }
 }
 
 async function addUser(user) {
@@ -15,6 +21,39 @@ async function addUser(user) {
     const db = await getDatabase();
 
     await db.collection("users").insertOne(user);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function getUserResponse(users, limit) {
+  const size = users.length;
+
+  return {
+    result: users,
+    metadata: {
+      seatsAvailable: size < limit,
+      size,
+    },
+  };
+}
+
+export async function getUsers() {
+  try {
+    const db = await getDatabase();
+
+    const users = await db.collection("users").find().toArray();
+
+    const managers = users.filter((user) => user.role === "dirigente");
+
+    const teamMembers = users.filter((user) => user.role === "equipista");
+
+    return JSON.parse(
+      JSON.stringify({
+        managers: getUserResponse(managers, 10),
+        teamMembers: getUserResponse(teamMembers, 2),
+      })
+    );
   } catch (e) {
     console.error(e);
   }
