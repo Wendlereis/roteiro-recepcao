800403b14384e7d0e0d58594b8f8071afa193347
Wendler Eis <wendler_eis@hotmail.com>
2024-09-05 20:39:02 -0300

    ♻️ Migrate calendar library (#27)
    * feat: add react-big-calendar
    
    * feat: add events on calendar
    
    * feat: add event color on event cell
    
    * feat: remove unused code
    
    * feat: format event time
    
    * feat: add admin permission when select an event
    
    * feat: refactor calendar from edition detail page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
-import { useState, useEffect } from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { useState, useEffect, useMemo } from "react";
 
 import Link from "next/link";
 
@@ -7,28 +9,29 @@ import { trpc } from "../ultis/trpc";
 import { Fab, Tab, Tabs } from "@mui/material";
 import { AddRounded } from "@mui/icons-material";
 
-import { ViewState } from "@devexpress/dx-react-scheduler";
-import {
-  Scheduler,
-  DayView,
-  Appointments,
-  CurrentTimeIndicator,
-} from "@devexpress/dx-react-scheduler-material-ui";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 
 import { useQuery } from "@tanstack/react-query";
-
-import { DayViewCell } from "../components/DayViewCell";
-import { DayViewLabel } from "../components/DayViewLabel";
-import { AppointmentItem } from "../components/AppointmentItem";
-import { AppointmentContent } from "../components/AppointmentContent";
 
 import { getEvents } from "../api/event";
 
 import { usePermission } from "../hook/usePermission";
 import { Menu } from "../components/Menu/Menu";
+import { ptBR } from "date-fns/locale";
+import {
+  format,
+  getDay,
+  parse,
+  setHours,
+  setMinutes,
+  startOfWeek,
+} from "date-fns";
+import { useRouter } from "next/router";
 
 export default function Schedule() {
   const { data: getEventsResponse } = useQuery(["events"], getEvents);
+
+  const { push } = useRouter();
 
   const edition = trpc.edition.getByActive.useQuery();
 
@@ -59,6 +62,34 @@ export default function Schedule() {
     return "loading...";
   }
 
+  const locales = {
+    "pt-BR": ptBR,
+  };
+
+  const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+  });
+
+  const events = getEventsResponse?.data.map((event) => ({
+    id: event._id,
+    title: event.title,
+    start: new Date(event.startDate),
+    end: new Date(event.endDate),
+    color: event.color,
+  }));
+
+  function handleOnSelectEvent(event) {
+    if (!isAdmin) {
+      return;
+    }
+
+    push(`/${event.id}/edit`);
+  }
+
   return (
     <div>
       <Menu label={edition.data.name} />
@@ -75,24 +106,28 @@ export default function Schedule() {
       </Tabs>
 
       {getEventsResponse?.data && (
-        <Scheduler data={getEventsResponse?.data}>
-          <ViewState currentDate={selectedTab} />
-
-          <DayView
-            startDayHour={7}
-            endDayHour={20.5}
-            cellDuration={10}
-            dayScaleCellComponent={DayViewCell}
-            timeScaleLabelComponent={DayViewLabel}
-          />
-
-          <Appointments
-            appointmentComponent={AppointmentItem}
-            appointmentContentComponent={AppointmentContent}
-          />
-
-          <CurrentTimeIndicator updateInterval={60000} />
-        </Scheduler>
+        <Calendar
+          style={{ height: "100vh" }}
+          localizer={localizer}
+          date={new Date(selectedTab)}
+          timeslots={1}
+          step={10}
+          min={setMinutes(setHours(new Date(selectedTab), 7), 0)}
+          max={setMinutes(setHours(new Date(selectedTab), 20), 40)}
+          defaultView="day"
+          events={events}
+          toolbar={false}
+          onSelectEvent={handleOnSelectEvent}
+          formats={{
+            eventTimeRangeFormat: (range) => `${format(range.start, "HH:mm")}`,
+          }}
+          eventPropGetter={(event) => ({
+            style: {
+              background: event.color,
+              borderColor: "transparent",
+            },
+          })}
+        />
       )}
 
       {isAdmin && (
