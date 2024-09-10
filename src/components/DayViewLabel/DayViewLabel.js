fabf48feb5e9c08526b2befb0887910edf732006
Wendler Eis <wendler_eis@hotmail.com>
2024-09-09 22:09:17 -0300

    feat: remove components (#40)

--- src/components/DayViewLabel/DayViewLabel.js
+++
@@ -1,9 +0,0 @@
-import { DayView } from "@devexpress/dx-react-scheduler-material-ui";
-
-export function DayViewLabel({ formatDate, ...restProps }) {
-  function handleFormatDate(date) {
-    return new Intl.DateTimeFormat("pt-BR", { timeStyle: "short" }).format(date);
-  }
-
-  return <DayView.TimeScaleLabel {...restProps} formatDate={handleFormatDate} />;
-}
