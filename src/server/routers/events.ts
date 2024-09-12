3862acb59bea8e4589e1bdbae922f9ae2204f28a
Wendler Eis <wendler_eis@hotmail.com>
2024-09-11 23:18:40 -0300

    feat: add route to list a past edition events by editionid

--- src/server/routers/events.ts
+++ src/server/routers/events.ts
@@ -1,5 +1,7 @@
 import { endOfDay } from "date-fns";
 
+import * as yup from "yup";
+
 import { procedure, router } from "../trpc";
 
 import * as editionRepository from "../repository/EditionRepository";
@@ -8,6 +10,18 @@ import * as eventRepository from "../repository/EventRepository";
 export const eventsRouter = router({
   get: procedure.query(async () => {
     const edition = await editionRepository.findByActive(true);
+
+    const { startDate, endDate } = edition;
+
+    const events = await eventRepository.findByDateRange(
+      startDate.toISOString(),
+      endOfDay(endDate).toISOString()
+    );
+
+    return { edition, events };
+  }),
+  getByEditionId: procedure.input(yup.string()).query(async (opts) => {
+    const edition = await editionRepository.findById(opts.input);
 
     const { startDate, endDate } = edition;
 
